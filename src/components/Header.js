edd1f0470f71c13af13bfeaf3adea9a1d30c0e7d
Cullen <zzyangzzy@hotmail.com>
2023-06-20 16:54:28 +0800

    Third Changes

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <header>
+            <title>Little Lemon</title>
+        </header>
+    )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+export default function Nav(){
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Menu</a>
+          </li>
+        </ul>
+      </nav>
+    );
+}
